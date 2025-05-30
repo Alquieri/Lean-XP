27552876e074fb1ef7b5b2bdfd901197f40e3592
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-05-30 20:24:33 -0300

    Deleted app and use main

--- leanxp.client/src/main.jsx
+++ leanxp.client/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Perfil from './Perfil';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/perfil" element={<Perfil />} />
+                <Route path="/sobre" element={<h1>Sobre</h1>} />
+            </Routes>
+        </BrowserRouter>
   </React.StrictMode>
 );
 
